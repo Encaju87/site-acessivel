c2688f4f8a1bb659f9319d42ecac149b468bccf5
Encaju87 <craveiro.luiz@escola.pr.gov.br>
2025-04-04 11:17:25 +0000

    Ajustando o menu de acessibilidade

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const menuAcessibilidade = document.getElementById('acessibilidade-opcoes');
-
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        if (menuAcessibilidade.style.display === 'block') {
+            menuAcessibilidade.style.display = 'none';
+        }
+        else {
+            menuAcessibilidade.style.display = 'block';
+        }
+    })
 
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
